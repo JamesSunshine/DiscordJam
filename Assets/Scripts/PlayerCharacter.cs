ecee43d3792424ea5a4c86c7b6d590153c8cfb03
James Sunshine <jamiecsunshine@gmail.com>
2019-03-02 16:25:01 +1300

    Initial Commit

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public float speed = 0;
+    public float health = 0;
+    public float money = 0;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+    }
+}
