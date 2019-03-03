69c4dcf0b2af5721392c6945a7a9ee2373b01d48
abel <abel465@gmail.com>
2019-03-03 16:34:59 +1300

    Add candle attack

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
-{
-
+public class Camera : MonoBehaviour {
     public Transform target;
 
     public float smoothSpeed = 0.125f;
--- Assets/Scripts/Candle.cs
+++ Assets/Scripts/Candle.cs
@@ -4,48 +4,60 @@ using UnityEngine;
 using Utility;
 
 public class Candle : UnityEngine.MonoBehaviour {
-    public double range;
-    public int damage;
-    public int attackSpeed;
-    
-    //public Vector2 position => transform.position;
-    public int health;
-    public Brightness brightness;
+	public double range;
+	public int damage;
+	public float attacksPerSecond;
+	public int health;
+	public Brightness brightness;
+	public GameObject[] gameObjects;
+	private GameObject projectile => gameObjects[0];
+	public Vector2 position => transform.position;
 
-    public Vector2 position {
-        get { return transform.position; }
-        
-    }
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
+	private long _lastAttackTime = 0;
+	private Enemy _enemy;
 
-    // Update is called once per frame
-    void Update() {
-        
-    }
+	// Start is called before the first frame update
+	void Start() {
+		_enemy = GameObject.Find("Enemy").GetComponent<Enemy>();
+	}
 
-    void OnHit(Enemy enemy) {
-        health -= 1; //enemy.damage
-        if (health <= 0) {
-            Destroy(this);
-        }
-    }
-    
-    Enemy GetClosestEnemy(Enemy[] enemies) {
-        Enemy closestEnemy = null;
-        float closestDistanceSqr = Mathf.Infinity;
-        Vector2 currentPosition = transform.position;
-        foreach (Enemy enemy in enemies) {
-            Vector2 directionToTarget = enemy.position - currentPosition;
-            float dSqrToTarget = directionToTarget.sqrMagnitude;
-            if (dSqrToTarget < closestDistanceSqr) {
-                closestDistanceSqr = dSqrToTarget;
-                closestEnemy = enemy;
-            }
-        }
-     
-        return closestEnemy;
-    }
+	// Update is called once per frame
+	void Update() {
+		long time = DateTime.Now.Ticks;
+		if (time - _lastAttackTime >= 10000000 / attacksPerSecond) {
+			Console.WriteLine(time);
+			_lastAttackTime = time;
+//			Enemy enemy = GetClosestEnemy(enemies);
+			Attack(_enemy);
+		}
+	}
+
+	void Attack(Enemy enemy) {
+		var startingPosition = position + new Vector2(0, 1.5f);
+		Instantiate(projectile, startingPosition, Quaternion.identity)
+			.GetComponent<Projectile>().direction = (enemy.position - startingPosition).normalized;
+	}
+
+	void OnHit(Enemy enemy) {
+		health -= 1; //enemy.damage
+		if (health <= 0) {
+			Destroy(this);
+		}
+	}
+	
+	Enemy GetClosestEnemy(Enemy[] enemies) {
+		Enemy closestEnemy = null;
+		float closestDistanceSqr = Mathf.Infinity;
+		Vector2 currentPosition = transform.position;
+		foreach (Enemy enemy in enemies) {
+			Vector2 directionToTarget = enemy.position - currentPosition;
+			float dSqrToTarget = directionToTarget.sqrMagnitude;
+			if (dSqrToTarget < closestDistanceSqr) {
+				closestDistanceSqr = dSqrToTarget;
+				closestEnemy = enemy;
+			}
+		}
+	 
+		return closestEnemy;
+	}
 }
