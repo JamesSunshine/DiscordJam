9f799ee2aef3d25e896bf42b34ba22374195d32c
abel <abel465@gmail.com>
2019-03-03 22:16:34 +1300

    Add GetFuturePosition to Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,65 +7,66 @@ using Vector2 = UnityEngine.Vector2;
 
 
 public class Enemy : MonoBehaviour {
-    public float speed = 6.0f;
-    public Vector2 position => transform.position;
+	public float speed = 6.0f;
+	public Vector2 position => transform.position;
 
 
-    private Rigidbody2D rb;
-    private Vector2 direction;
-    private GameObject player;
+	private Rigidbody2D rb;
+	private Vector2 direction;
+	private GameObject player;
 
-    // Start is called before the first frame update
-    void Start() {
-        rb = GetComponent<Rigidbody2D>();
+	// Start is called before the first frame update
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
 
-        direction = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
-        rb.AddForce(direction * speed);
+		direction = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+		rb.AddForce(direction * speed);
 
-        player = GameObject.Find("Player");
-    }
+		player = GameObject.Find("Player");
+	}
 
-    // Update is called once per frame
-    void FixedUpdate() {
-        UpdateFlightPitch();
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "projectile")
-        {
-            Destroy(collision.gameObject);
-            Destroy(this.gameObject);
-        }
-    }
-    void UpdateFlightPitch()
-    {
-        Vector2 correctionForce = FlightCorrectionForce(player.transform.position);
-        rb.AddForce(correctionForce);
+	// Update is called once per frame
+	void FixedUpdate() {
+		UpdateFlightPitch();
+	}
 
-        rb.velocity = rb.velocity.normalized * speed;
-    }
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.CompareTag("projectile")) {
+			Destroy(collision.gameObject);
+			Destroy(this.gameObject);
+		}
+	}
 
-    Vector2 getPosition()
-    {
-        Vector3 pos = transform.position;
-        return new Vector2(pos.x, pos.y);
-    }
+	void UpdateFlightPitch() {
+		Vector2 correctionForce = FlightCorrectionForce(player.transform.position);
+		rb.AddForce(correctionForce);
 
+		rb.velocity = rb.velocity.normalized * speed;
+	}
 
-    Vector2 FlightCorrectionForce(Vector2 target) {
-        Vector2 turnForce = Vector2.Perpendicular(rb.velocity) / rb.mass;
-        float angleToTarget = Vector2.Angle(rb.velocity, target - getPosition());
-        
-        if (angleToTarget > 180) {
-            if (angleToTarget > (180 - 37))
-            {
-                return turnForce * -1;
-            }
-        }
-        else if (angleToTarget > 37)
-        {
-            return turnForce;
-        }
-        return Vector2.zero;
-    }
+	Vector2 getPosition() {
+		Vector3 pos = transform.position;
+		return new Vector2(pos.x, pos.y);
+	}
+
+	public Vector2 GetFuturePosition() {
+		return position + speed * direction / 0.1f;
+	}
+
+	Vector2 FlightCorrectionForce(Vector2 target) {
+		Vector2 turnForce = Vector2.Perpendicular(rb.velocity) / rb.mass;
+		float angleToTarget = Vector2.Angle(rb.velocity, target - getPosition());
+		
+		if (angleToTarget > 180) {
+			if (angleToTarget > (180 - 37))
+			{
+				return turnForce * -1;
+			}
+		}
+		else if (angleToTarget > 37)
+		{
+			return turnForce;
+		}
+		return Vector2.zero;
+	}
 }
